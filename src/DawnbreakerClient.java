554e4ebd29ffef1c6eb75944bb6775f1c63d9f8e
mathias124 <mfs2001@hotmail.dk>
2023-03-13 23:36:09 +0100

    Finished Version of the Application! It works Version 1.0 Released. Now you can continue using it and sending messages after! It will start a new request.

--- src/DawnbreakerClient.java
+++ src/DawnbreakerClient.java
@@ -1,5 +1,8 @@
 import controlP5.*;
 import processing.core.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 public class DawnbreakerClient extends PApplet {
     UI uI;
@@ -8,12 +11,26 @@ public class DawnbreakerClient extends PApplet {
     String inputRCPT;
     String message;
     String getMessage;
+    String Subject;
+     boolean pressed = false;
+    //boolean pressed = false;
 
     public static void main(String args[]) {
         PApplet.main("DawnbreakerClient");
-    }
 
-    @Override
+    }
+        Scanner in = new Scanner(System.in);
+        String startSyntax = "helo martin";
+        String senderSyntax = "mail from: ";
+        String recipientSyntax = "rcpt to: ";
+        String messageSyntax = "data";
+        String quitSyntax = "quit";
+
+
+        //boolean pressed = false;
+
+
+
     public void settings() {
         size(1000, 500);
     }
@@ -29,16 +46,109 @@ public class DawnbreakerClient extends PApplet {
     }
 
     public void draw() {
-
+        text("Group 29 Email Services!" ,0,0);
+        fill(133,177,177);
+        //inputSender = cp5.get(Textfield.class, "Sender").getText();
     }
 
+    public void Run(){
+        try {
+
+            Socket socketClient = new Socket("datacomm.bhsi.xyz", 2552);
+
+            System.out.println("[CONNECTION] Connection Initiating...\n");
+
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
+
+            BufferedWriter writer =
+                    new BufferedWriter(new OutputStreamWriter(socketClient.getOutputStream()));
+
+            String s, serverMsg;
+
+            while ((serverMsg = reader.readLine()) != null) {
+                System.out.println(serverMsg);
+                if (serverMsg.startsWith("220")) {
+                    System.out.println("[SERVER RESPONSE] Connection Established\n");
+                    writer.write(startSyntax + "\r\n");
+                    writer.flush();
+
+                } else if (serverMsg.startsWith("250 comit")) {
+                    System.out.println("[SERVER RESPONSE] Successful Login");
+                    System.out.println("\nPlease enter the sender email address");
+                    //String senderInput = in.nextLine();
+
+
+                        writer.write(senderSyntax + inputSender+ "\r\n");
+                        System.out.println(inputSender);
+                        writer.flush();
+
+
+                } else if (serverMsg.startsWith("250 2.1.0")) {
+                    System.out.println("\nPlease enter a recipient email address");
+                    writer.write(recipientSyntax + inputRCPT + "\r\n");
+                    System.out.println(inputRCPT);
+                    writer.flush();
+
+                } else if (serverMsg.startsWith("250 2.1.5")) {
+                    writer.write(messageSyntax  + "\r\n");
+                    writer.flush();
+                    System.out.print("\n");
+
+                } else if (serverMsg.startsWith("354")) {
+                    System.out.println("[SERVER RESPONSE] Sender and Recipient Email Addresses Confirmed");
+                    System.out.println("\n[SERVER RESPONSE] Proceed with the Email Content");
+
+                    // taking input for mail subject
+                    System.out.println("Please enter the mail subject");
+                    //writer.write(Subject + "\r\n");
+                    //Scanner subject = new Scanner(System.in);
+                    //String mailSubject = subject.nextLine();
+
+                    // taking input for mail content
+                    System.out.println("\nPlease enter the mail content");
+                    System.out.println("End the message with a \".\"");
+                    Scanner body = new Scanner(System.in);
+
+                    String mail = "Subject: " + Subject + "\n" + message;
+
+                    writer.write(mail + "\r\n");
+                    writer.write("." + "\r\n");
+                    writer.flush();
+                    System.out.print("\n");
+
+                } else if (serverMsg.startsWith("250 2.0.0 Ok: queued")) {
+                    writer.write(quitSyntax + "\r\n");
+                    writer.flush();
+                    System.out.println("[SERVER RESPONSE] Success! Email is being sent out to Recipient");
+                    System.out.print("\n");
+
+                } else if (serverMsg.startsWith("221 2.0.0")) {
+                    System.out.println("[CONNECTION] Connection Terminated");
+
+                } else {
+                    s = in.nextLine();
+                    writer.write(s + "\r\n");
+                    writer.flush();
+                }
+            }
+
+        } catch (UnknownHostException ex) {
+            throw new RuntimeException(ex);
+        } catch (IOException ex) {
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public void TestB(){
-    cp5.addButton("Test")
-                .setPosition(width/2 -300, height/2-75)
+    cp5.addButton("SendMessage")
+                .setPosition(width/2 -100, height/2)
                 .setSize(182,45)
                 .setColorValue(0)
-                .setColorLabel(133)
+                .setColorLabel(ControlP5Constants.ORANGE)
                 .setColorActive(155)
             .setColorBackground(177)
             ;
@@ -48,7 +158,7 @@ public class DawnbreakerClient extends PApplet {
                 .setSize(282, 62)
                 .setAutoClear(false)
                 .setColorLabel(000000)
-                .setAutoClear(true)
+                .setAutoClear(pressed)
         ;
         cp5.addTextfield("Recept")
                 .setCaptionLabel("Reciever")
@@ -57,25 +167,39 @@ public class DawnbreakerClient extends PApplet {
                 .setSize(282, 62)
                 .setAutoClear(false)
                 .setColorLabel(000000)
-                .setAutoClear(true)
+                .setAutoClear(pressed)
         ;
         cp5.addTextfield("Message")
                 .setCaptionLabel("Message")
+                .setPosition(width/2 -300, height/2-75)
+                .setSize(580, 62)
+                .setAutoClear(false)
+                .setColorLabel(000000)
+                .setColorValue(ControlP5Constants.GREEN)
+                .setAutoClear(pressed)
+        ;
+        cp5.addTextfield("Subject")
+                .setCaptionLabel("Subject")
                 .setPosition(width/2 -300, height/2 -150)
                 .setSize(580, 62)
                 .setAutoClear(false)
                 .setColorLabel(000000)
-                .setColorValue(133)
-                .setAutoClear(true)
+                .setColorValue(ControlP5Constants.GREEN)
+                .setAutoClear(pressed)
         ;
 }
-public void Test(){
+public void SendMessage(){
     inputSender = cp5.get(Textfield.class, "Sender").getText();
     inputRCPT = cp5.get(Textfield.class,"Recept").getText();
     message = cp5.get(Textfield.class,"Message").getText();
+    Subject = cp5.get(Textfield.class,"Subject").getText();
 
-    getMessage = message;
-    text("Du har vundet en kage"+getMessage,0,0);
-    System.out.println("test"+inputRCPT + inputSender);
+    text("Du har vundet en kage"+getMessage,width/2,height/2-50);
+    fill(255,100,33);
+    Run();
+    cp5.get(Textfield.class, "Sender").clear();
+    cp5.get(Textfield.class,"Recept").clear();
+    cp5.get(Textfield.class,"Message").clear();
+    cp5.get(Textfield.class,"Subject").clear();
 }
 }
